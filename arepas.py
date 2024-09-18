b949c296b288f70aed433f4e87b6597062894862
gloriafer2 <gloria.fernandes@correo.unimet.edu.ve>
2024-09-18 17:26:36 -0400

    Initial commit

---
+++ arepas.py
@@ -0,0 +1,21 @@
+#arepasaguna = float(input('Ingrese la cantidad de agua: '))
+agua = 1
+harina = 1
+sal= 1/16/3
+aceite = 1/16
+
+
+agua = int(input('Ingrese la cantidad de agua: '))
+harina = int(input('Ingrese la cantidad de harina: '))
+sal = float(input('Ingrese cucharadas de sal: '))//16//3
+aceite = float(input('Ingrese la cantidad de aceite: '))//16
+
+
+bol = agua + harina + sal
+budare = bol + aceite
+
+print('El agua es', agua)
+print('La harina es', harina)
+print('La sal es', sal)
+print('El aceite es', aceite)
+print('La arepa final es', budare)
